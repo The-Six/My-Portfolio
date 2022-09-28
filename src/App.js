b9f11f55cc381750c77bf59c6d74ae3035623a2b
The-Six <ijazdriscoll@gmail.com>
2022-09-28 14:03:13 +0100

    projects component added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import Navbar from "./components/Navbar/navbar";
 import Intro from "./components/Intro/intro";
 import About from "./components/About/about";
+import Projects from "./components/Projects/projects";
+
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <Navbar />
       <Intro />
       <About />
+      <Projects />
     </div>
   );
 }
