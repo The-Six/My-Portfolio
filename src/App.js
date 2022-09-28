d3680962480c0246bb849c4ff9527e121f301b43
The-Six <ijazdriscoll@gmail.com>
2022-09-28 13:55:47 +0100

    created react app and working on completing the rest of the components

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Navbar from "./components/Navbar/navbar";
+import Intro from "./components/Intro/intro";
+import About from "./components/About/about";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Intro />
+      <About />
+    </div>
+  );
+}
+
+export default App;
