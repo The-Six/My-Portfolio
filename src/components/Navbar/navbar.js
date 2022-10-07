14b65d660d28a27d2dc9a5d65c9372dcc746f5e6
The-Six <ijazdriscoll@gmail.com>
2022-10-07 17:28:06 +0100

    added css to footer & Navbar

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -2,7 +2,13 @@ import React from "react";
 import "./navbar.css";
 
 function Navbar() {
-  return <div className="Navbar">navbar</div>;
+  return <div className="Navbar">
+    <button className="intro_tab"></button>
+    <button className="about_tab"></button>
+    <button className="projects_tab"></button>
+    <button className="skills_tab"></button>
+    <button className="contact_tab"></button>
+  </div>;
 }
 
 export default Navbar;
