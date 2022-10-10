ee12c4dd56979a18477732f45e639816197a3392
Scott Wilson <swrs@hotmail.co.uk>
2022-10-10 15:34:12 +0100

    added selector tab functionality and moved states to app level

--- src/App.js
+++ src/App.js
@@ -7,20 +7,33 @@ import Skills from "./components/Skills/skills";
 import Contact from "./components/Contact/contact";
 import Footer from "./components/Footer/footer";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [intro, setIntro] = useState("intro_tab");
+  const [about, setAbout] = useState("about_tab");
+  const [projects, setProjects] = useState("projects_tab");
+  const [skills, setSkills] = useState("skills_tab");
+  const [contact, setContact] = useState("contact_tab");
   return (
     <div className="App">
-      <Navbar />
-      <main className="Main">
-      <section className="Intro_about">
+      <Navbar
+        intro={intro}
+        about={about}
+        projects={projects}
+        skills={skills}
+        contact={contact}
+        setIntro={setIntro}
+        setAbout={setAbout}
+        setProjects={setProjects}
+        setSkills={setSkills}
+        setContact={setContact}
+      />
       <Intro />
       <About />
-      </section>
       <Projects />
       <Skills />
-      <Contact/>
-      </main>
+      <Contact />
       <Footer />
     </div>
   );
--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,22 +1,65 @@
 import React from "react";
 import "./navbar.css";
-import {useState} from "react";
+import { useState } from "react";
 
+function Navbar({
+  intro,
+  about,
+  projects,
+  skills,
+  contact,
+  setIntro,
+  setAbout,
+  setProjects,
+  setSkills,
+  setContact,
+}) {
+  function setStyles(input) {
+    if (input === 1) {
+      setIntro("selector_tab");
+      setAbout("about_tab");
+      setProjects("projects_tab");
+      setSkills("skills_tab");
+      setContact("contact_tab");
+    } else if (input === 2) {
+      setIntro("intro_tab");
+      setAbout("selector_tab");
+      setProjects("projects_tab");
+      setSkills("skills_tab");
+      setContact("contact_tab");
+    } else if (input === 3) {
+      setIntro("intro_tab");
+      setAbout("about_tab");
+      setProjects("selector_tab");
+      setSkills("skills_tab");
+      setContact("contact_tab");
+    } else if (input === 4) {
+      setIntro("intro_tab");
+      setAbout("about_tab");
+      setProjects("projects_tab");
+      setSkills("selector_tab");
+      setContact("contact_tab");
+    } else if (input === 5) {
+      setIntro("intro_tab");
+      setAbout("about_tab");
+      setProjects("projects_tab");
+      setSkills("skills_tab");
+      setContact("selector_tab");
+    }
+  }
 
-function Navbar() {
-  const [selected, setSelected] = useState("selector_tab")
   return (
     <div className="Navbar">
       <div className="inbetween_box"></div>
-      <button className="intro_tab"></button>
+      <button className={intro} onClick={() => setStyles(1)}></button>
       <div className="inbetween_box"></div>
-      <button className="about_tab"></button>
+      <button className={about} onClick={() => setStyles(2)}></button>
       <div className="inbetween_box"></div>
-      <button className="projects_tab"></button>
+      <button className={projects} onClick={() => setStyles(3)}></button>
       <div className="inbetween_box"></div>
-      <button className="skills_tab"></button>
+      <button className={skills} onClick={() => setStyles(4)}></button>
       <div className="inbetween_box"></div>
-      <button className="contact_tab"></button>
+      <button className={contact} onClick={() => setStyles(5)}></button>
       <div className="inbetween_box"></div>
     </div>
   );
