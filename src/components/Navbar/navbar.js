7c87d89dc70e00ce343d3515cb07b66a4131a482
The-Six <ijazdriscoll@gmail.com>
2022-10-10 13:28:25 +0100

    added selector_tab to further create a pip-boy UI when selecting different boxes

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -2,13 +2,21 @@ import React from "react";
 import "./navbar.css";
 
 function Navbar() {
-  return <div className="Navbar">
-    <button className="intro_tab"></button>
-    <button className="about_tab"></button>
-    <button className="projects_tab"></button>
-    <button className="skills_tab"></button>
-    <button className="contact_tab"></button>
-  </div>;
+  return (
+    <div className="Navbar">
+      <div className="inbetween_box"></div>
+      <button className="selector_tab"></button>
+      <div className="inbetween_box"></div>
+      <button className="about_tab"></button>
+      <div className="inbetween_box"></div>
+      <button className="projects_tab"></button>
+      <div className="inbetween_box"></div>
+      <button className="skills_tab"></button>
+      <div className="inbetween_box"></div>
+      <button className="contact_tab"></button>
+      <div className="inbetween_box"></div>
+    </div>
+  );
 }
 
 export default Navbar;
