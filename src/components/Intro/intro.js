01ae35978d54adf35afbd4a8728741cffd6ffe78
The-Six <ijazdriscoll@gmail.com>
2022-11-19 15:29:48 +0000

    quote logic completed on intro page for desktop view

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -10,13 +10,27 @@ function Intro() {
         <div className="right_introBar"></div>
       </div>
       <div className="introCenterSection">
-      <div className="spacerSection"></div>
-      <div className="intro_textBox">
+        <div className="spacerSection"></div>
+        <div className="intro_textBox">
           <h1>My Name Is Ijaz Driscoll</h1>
           <p>Full Stack Software Engineer</p>
           <p>Based in London, UK.</p>
-      </div>
-      <div className="quoteSection"></div>
+        </div>
+        <div className="quoteSection">
+          <div className="textContainer">
+            <p class="typing">
+              <p className="textLine">"It's still the same old story</p>
+              <p className="textLine">A fight for love and glory</p> 
+              <p className="textLine">A case of do or die...</p>
+            </p>
+
+            <div class="hiders">
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="vaultLogo"></div>
     </div>
