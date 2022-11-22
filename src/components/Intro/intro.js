9a85cf241939c8fcdc35d2be056c67bcdfc52f23
The-Six <ijazdriscoll@gmail.com>
2022-11-22 16:13:22 +0000

    I have changed the intro page to just say Ijaz Driscoll as it is implicit

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -12,7 +12,7 @@ function Intro() {
       <div className="introCenterSection">
         <div className="spacerSection"></div>
         <div className="intro_textBox">
-          <h1>My Name Is Ijaz Driscoll</h1>
+          <h1>Ijaz Driscoll</h1>
           <p>Full Stack Software Engineer</p>
           <p>Based in London, UK.</p>
         </div>
