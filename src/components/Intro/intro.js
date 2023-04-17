ff05f630a101c8deeab09a0f6d168f7ddb04c618
The-Six <ijazdriscoll@gmail.com>
2023-04-17 15:10:47 +0100

    I have moved the vitals monitor to be under the left intro bar to match where the location of the monitor on projects

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -6,13 +6,14 @@ function Intro() {
   return (
     <div className="Intro">
       <div className="intro_box">
-        <div className="left_introBar"></div>
+        <div className="left_introBar">
+        <p><VitalSignsMonitor /></p>
+        </div>
         <div className="intro_text">W E L C O M E</div>
         <div className="right_introBar"></div>
       </div>
       <div className="introCenterSection">
         <div className="spacerSection"> 
-        <p><VitalSignsMonitor /></p>
         </div>
         <div className="intro_textBox">
           <h2>Ijaz Driscoll</h2>
